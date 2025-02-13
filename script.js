37483baa547256af4f07225348b067ae1cc9ae3e
Laraib <laraib.yousfani@gmail.com>
2025-02-13 09:41:22 -0800

    Fixed style text inside the contact button and formatted accordingly.

--- script.js
+++ script.js
@@ -32,16 +32,3 @@ function closeContact(contactme) {
     contactme.classList.remove("active");
     overlay.classList.remove("active");
 }
-
-document.getElementById('submitButton').addEventListener('click', function () {
-    var form = document.getElementById('contactForm');
-    
-    if (form.checkValidity()) {
-        // If all fields are valid, submit the form (you can add actual form submission logic here)
-        alert('Form Submitted');
-        form.reset(); // Optional: reset form after submission
-    } else {
-        // If form is not valid, trigger the HTML5 validation message
-        form.reportValidity();
-    }
-});
